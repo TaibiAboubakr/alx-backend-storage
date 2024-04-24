1ff39f357f07abb6d354f0279525021f65080681
aboubakr <c.aboubakr@gmail.com>
2024-04-24 23:33:45 +0100

    update the file 0-uniq_users.sql

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,9 +1,10 @@
+-- Active: 1699973134556@@127.0.0.1@3306@testdb
 -- creates a table users
 -- create a table users
-CREATE TABLE IF NOT EXIST users
+CREATE TABLE IF NOT EXISTS users
 (
 id INT AUTO_INCREMENT PRIMARY KEY,
 email VARCHAR(255) NOT NULL UNIQUE,
-name VARCHAR(255)
+name VARCHAR(255),
 country ENUM('US', 'CO', 'TN') NOT NULL DEFAULT 'US'
 );
