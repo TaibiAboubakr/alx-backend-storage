071d6330635320b0820710823374c5a44f0043b3
aboubakr <c.aboubakr@gmail.com>
2024-04-24 23:39:16 +0100

    add the file 0-uniq_users.sql

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,9 +1,11 @@
 -- creates a table users
+-- id, integer, never null, auto increment and primary key
+-- email, string (255 characters), never null and unique
+-- name, string (255 characters)
 -- create a table users
 CREATE TABLE IF NOT EXISTS users
 (
 id INT AUTO_INCREMENT PRIMARY KEY,
 email VARCHAR(255) NOT NULL UNIQUE,
-name VARCHAR(255),
-country ENUM('US', 'CO', 'TN') NOT NULL DEFAULT 'US'
+name VARCHAR(255)
 );
