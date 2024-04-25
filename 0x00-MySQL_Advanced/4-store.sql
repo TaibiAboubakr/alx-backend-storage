10fbd351f0571b3ac39cfe032319e945d7f84d7d
aboubakr <c.aboubakr@gmail.com>
2024-04-25 22:45:25 +0100

    add the file 4-store.sql

--- 0x00-MySQL_Advanced/4-store.sql
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -1,10 +1,11 @@
 -- Create the trigger
+DROP TRIGGER IF EXISTS decrease_quantity_after_order;
 CREATE TRIGGER decrease_quantity_after_order
 AFTER INSERT ON orders
 FOR EACH ROW
 BEGIN
-    -- Decrease the quantity of the ordered item in the items table
     UPDATE items
-    SET quantity = quantity - NEW.quantity_ordered
-    WHERE item_id = NEW.item_id;
+        SET quantity = quantity - NEW.number
+        WHERE name = NEW.item_name;
 END;
+
