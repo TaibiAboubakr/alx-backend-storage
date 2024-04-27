23b2e6d1735f769d7d70f6a9bdec1e6d3e12ea6f
aboubakr <c.aboubakr@gmail.com>
2024-04-27 21:48:08 +0100

    update the file 7-average_score.sql

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -12,7 +12,7 @@ CREATE PROCEDURE ComputeAverageScoreForUser(
 	IN user_id int)
 BEGIN
  	DECLARE clc_score DECIMAL;
-	SELECT AVG(score) INTO clc_score from corrections  WHERE user_id = user_id;
+	SELECT AVG(score) INTO clc_score from corrections WHERE corrections.user_id = user_id;
 	UPDATE users SET average_score = clc_score WHERE id = user_id;
 END $$
 DELIMITER ;
