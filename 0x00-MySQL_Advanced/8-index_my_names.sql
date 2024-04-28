ba147423616edb64131cf44ed09563c3d365d943
aboubakr <c.aboubakr@gmail.com>
2024-04-28 14:48:12 +0100

    update the file 8-index_my_names.sql

--- 0x00-MySQL_Advanced/8-index_my_names.sql
+++ 0x00-MySQL_Advanced/8-index_my_names.sql
@@ -1,14 +1,9 @@
 -- a SQL script that creates an index idx_name_first on the table names and the first letter of name.
 /* Requirements:
+
 Import this table dump: names.sql.zip
 Only the first letter of name must be indexed
 */
--- Add a new column to store the first letter
-ALTER TABLE names
-ADD COLUMN first_letter_name CHAR(1);
-
--- Step 2: Populate the new column with the first letter of the original column
-UPDATE names
-SET first_letter_name = LEFT(name, 1);
 -- creates an index idx_name_first on the table names
-CREATE INDEX idx_name_first ON names(first_letter_name);
+CREATE INDEX idx_name_first
+ON names(name(1));
