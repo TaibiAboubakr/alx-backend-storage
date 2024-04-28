d251ee510be2597e4c78007c03b6e438065fc2ae
aboubakr <c.aboubakr@gmail.com>
2024-04-28 14:52:23 +0100

    update the file 9-index_name_score.sql

--- 0x00-MySQL_Advanced/9-index_name_score.sql
+++ 0x00-MySQL_Advanced/9-index_name_score.sql
@@ -1,9 +1,10 @@
--- a SQL script that creates an index idx_name_first on the table names and the first letter of name.
-/* Requirements:
-
+-- a SQL script that creates an index idx_name_first_score on the table names and the first letter of name and the score.
+/*
+Requirements:
 Import this table dump: names.sql.zip
-Only the first letter of name must be indexed
+Only the first letter of name AND score must be indexed
 */
--- creates an index idx_name_first on the table names
-CREATE INDEX idx_name_first
-ON names(name(1));
+-- creates an index idx_name_first_score on the table names
+CREATE INDEX idx_name_first_score
+ON names(name(1), score);
+SHOW INDEX from names;
