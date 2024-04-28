05412120d0bf1ded224f38a1290101544502eef1
aboubakr <c.aboubakr@gmail.com>
2024-04-28 15:16:15 +0100

    update the file 10-div.sql

--- 0x00-MySQL_Advanced/10-div.sql
+++ 0x00-MySQL_Advanced/10-div.sql
@@ -13,9 +13,9 @@ DELIMITER $$
 DROP FUNCTION IF EXISTS SafeDiv;
 -- function SafeDiv that divides (and returns) the first by the second number or returns 0 if the second number is equal to 0.
 CREATE FUNCTION SafeDiv(a INT, b INT)
-RETURNS DECIMAL DETERMINISTIC
+RETURNS FLOAT DETERMINISTIC
 BEGIN
-    DECLARE result DECIMAL;
+    DECLARE result FLOAT;
 	SET result = 0;
 	IF b = 0 THEN
 		RETURN result;
@@ -23,5 +23,5 @@ BEGIN
     SET result = (a/b) ;
     RETURN result;
 END$$
-
 DELIMITER ;
+SELECT SafeDiv(6, 4);
