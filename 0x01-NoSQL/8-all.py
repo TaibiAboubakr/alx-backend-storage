5dfbcc1a3186d84c8baf94983d48b118ecc956cc
aboubakr <c.aboubakr@gmail.com>
2024-04-29 23:44:47 +0100

    update the file 8-all.py

--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python3
 """ list all """
+
+
 def list_all(mongo_collection):
+    """ list all documents """
     all_documents = list(mongo_collection.find())
     return all_documents
