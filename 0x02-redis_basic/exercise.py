2983523ed17e095767c3aadc2184f60c88dc37b6
aboubakr <c.aboubakr@gmail.com>
2024-05-01 17:31:19 +0100

    add the file exercise.py

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -3,6 +3,7 @@
 import redis
 import uuid
 from typing import Union
+from typing import Callable
 
 
 class Cache():
@@ -11,9 +12,28 @@ class Cache():
         self._redis = redis.Redis()
         self._redis.flushdb()
 
-
-    def store(self, data :  Union[str, bytes, int, float]) -> str:
+    def store(self, data:  Union[str, bytes, int, float]) -> str:
         """ store method """
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(
+            self,
+            key: str, fn: Callable = None) -> Union[str, bytes, int, float]:
+        """ Retrieves a value from Redis storage based on a given key
+        """
+        data = self._redis.get(key)
+        if fn:
+            return fn(data)
+        return data
+
+    def get_str(self, key: str) -> str:
+        '''Retrieves a str value from Redis storage based on a given key.
+        '''
+        return self.get(key, lambda x: x.decode('utf-8'))
+
+    def get_int(self, key: str) -> int:
+        '''Retrieves an int value from Redis storage based on a given key.
+        '''
+        return self.get(key, lambda x: int(x))
