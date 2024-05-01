4c436f1908088dcceeec3478486954bbc42485dd
aboubakr <c.aboubakr@gmail.com>
2024-05-01 17:47:23 +0100

    update the file web.py

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,33 +1,27 @@
 #!/usr/bin/env python3
-"""module with tools for request caching and tracking.
-"""
+"""Module with tools for request caching and tracking."""
 import redis
 import requests
 from functools import wraps
 from typing import Callable
 
-
 redis_client = redis.Redis()
-"""The module-level Redis instance.
-"""
+"""The module-level Redis instance."""
 
 
 def data_cacher(method: Callable) -> Callable:
-    """Caches the output of fetched data.
-    """
+    """Caches the output of fetched data."""
     @wraps(method)
-    def invoker(url: str) -> str:
-        """wrapper function for cache.
-        """
+    def wrapper(url: str) -> str:
+        """Wrapper function for caching."""
         redis_client.incr(f'count:{url}')
         result = redis_client.get(f'result:{url}')
         if result:
             return result.decode('utf-8')
         result = method(url)
-        redis_client.set(f'count:{url}', 0)
-        redis_client.setex(f'result:{url}', 10, result)
+        redis_client.set(f'result:{url}', result, ex=10)  # Using 'ex' parameter for expiration time
         return result
-    return invoker
+    return wrapper
 
 
 @data_cacher
@@ -35,6 +29,5 @@ def get_page(url: str) -> str:
     """Returns the content of a URL after caching the request's response,
     and tracking the request.
     """
-    cached_content = redis_client.get(url)
-    if cached_content:
-        return cached_content.decode('utf-8')
+    response = requests.get(url)
+    return response.text
